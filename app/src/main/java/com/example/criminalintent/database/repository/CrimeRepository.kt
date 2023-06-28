6792abecc87f09d425ff9a86963ce61bc24fedf8
DanilloSantosTi <danillomdsti@gmail.com>
2023-06-28 02:06:40 -0300

    [main] feature - create access to database

--- app/src/main/java/com/example/criminalintent/database/repository/CrimeRepository.kt
+++ app/src/main/java/com/example/criminalintent/database/repository/CrimeRepository.kt
@@ -1,8 +1,26 @@
 package com.example.criminalintent.database.repository
 
 import android.content.Context
+import androidx.room.Room
+import com.example.criminalintent.database.CrimeDatabase
+import com.example.criminalintent.model.Crime
+import java.util.UUID
+
+private const val DATABASE_NAME = "crime-database"
 
 class CrimeRepository private constructor(context: Context) {
+
+    private val database: CrimeDatabase = Room
+        .databaseBuilder(
+            context.applicationContext,
+            CrimeDatabase::class.java,
+            DATABASE_NAME
+        )
+        .build()
+
+    suspend fun getCrimes(): List<Crime> = database.crimeDao().getCrimes()
+
+    suspend fun getCrime(id: UUID): Crime = database.crimeDao().getCrime(id)
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
